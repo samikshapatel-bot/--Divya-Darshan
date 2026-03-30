8d74b6952b037b590a53b379eef725b8ccb5f965
Samiksha PATEL <samikshapatel780@gmail.com>
2026-03-30 19:43:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -159,4 +159,5 @@ app.get('/admin-dashboard', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
